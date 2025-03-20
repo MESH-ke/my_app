151217f08ec1fcd6fa741016ec4ba80958ce9529
MESH-ke <Kemboim36@gmail.com>
2025-03-20 08:18:55 +0300

    Update AddProducts.jsx

--- AddProducts.jsx
+++ AddProducts.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, Link} from "react";
 
 const AddProducts = () => {
     let [product_name, setProductName] =useState("");
@@ -15,7 +15,7 @@ const AddProducts = () => {
         try {
             setError("")
             setSuccess("")
-            setLoading("Please wait as we subbmit your data")
+            setLoading("Please wait as we submit your data")
             
             const data=new FormData();
             data.append("product_name", product_name);
@@ -23,7 +23,7 @@ const AddProducts = () => {
             data.append("product_cost", product_cost);
             data.append("product_photo", product_photo);
 
-            const response =await axios.post("https://ndege25.pythonanywhere.com/api/addproduct", data)
+            const response =await axios.post("https://Mesh1.pythonanywhere.com/api/addproducts", data)
             setLoading("")
             setSuccess(response.data.success)
             setProductName("")
@@ -35,6 +35,13 @@ const AddProducts = () => {
         }
     }
     return ( 
+    <div className="container-fluid">
+                <nav className="m-4">
+                <Link className="btn-btn dark mx-2" to ="/">Home</Link>
+                <Link className="btn-btn dark mx-2" to ="/signin">Sign In</Link>
+                <Link className="btn-btn dark mx-2" to ="/signup">Sign Up</Link>
+                <Link className="btn-btn dark mx-2" to ="/addproducts">Add Products</Link>
+            </nav>
       <div className="row justify-content-center mt-4">
         <div className="col-md-6 card shadow p-4">
             <h2>Add products</h2>
@@ -47,9 +54,10 @@ const AddProducts = () => {
                 <input type="number" className="form-control" placeholder="Product Cost" required value={product_cost} onChange={(e)=> setProductCost(e.target.value)} /> <br />
                 <label htmlFor="" className="form-label">Product Photo</label> <br />
                 <input type="file" className="form-control" onChange={(e)=> SetProductPhoto(e.target.files[0])} /> <br />
-        <button className="btn btn-primary">Add Products</button>
+                <button className="btn btn-primary">Add Products</button>
             </form>
         </div>
+    </div>
       </div>
      );
 }
